ce5f027a826cbb23b08301f59658cd9ba9a00b3d
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-03-12 03:33:48 +0600

    creating blogs using Next.js Form component

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,11 @@
+import BlogForm from "@/components/ui/BlogForm";
+
+const CreateBlogPage = () =>{
+   return (
+    <div>
+        <h1>Create Blog Page</h1>
+        <BlogForm/>
+    </div>
+   )
+}
+export default CreateBlogPage;
