ff9f6d24525d0ddba64d35ede1394bc7636b930b
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-03-13 03:46:11 +0600

    Exploring Route Handlers and building Restful APIs

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,6 +1,18 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
 import { Blog } from "@/types";
-
+export async function generateMetadata ({
+    params,
+} : {
+    params : Promise<{blogId : string}>
+}) {
+    const {blogId} = await params;
+    const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+    const blog = await res.json();
+    return {
+        title: blog.title,
+        description : blog?.description
+    }
+}
 export const generateStaticParams = async()=>{
     const res = await fetch("http://localhost:5000/blogs");
     const blogs = await res.json();
